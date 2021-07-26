1774814ae6a82676670379a78bf3f7d69dd5e4d4
Kal <info@kalacademy.org>
2021-07-26 05:35:28 -0700

    Catalog api

--- ProductCatalogAPI/Controllers/CatalogController.cs
+++ ProductCatalogAPI/Controllers/CatalogController.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ProductCatalogAPI.Data;
+using ProductCatalogAPI.Domain;
+using ProductCatalogAPI.ViewModels;
 
 namespace ProductCatalogAPI.Controllers
 {
@@ -11,5 +16,43 @@ namespace ProductCatalogAPI.Controllers
     [ApiController]
     public class CatalogController : ControllerBase
     {
+        private readonly CatalogContext _context;
+        private readonly IConfiguration _config;
+        public CatalogController(CatalogContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Items(
+            [FromQuery]int pageIndex = 0, 
+            [FromQuery]int pageSize = 6)
+        {
+            var itemsCount = _context.Catalog.LongCountAsync();
+            var items = await _context.Catalog
+                .OrderBy(c => c.Name)
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            items = ChangePictureUrl(items);
+            var model = new PaginatedItemsViewModel
+            {
+                PageIndex = pageIndex,
+                PageSize = items.Count,
+                Count = itemsCount.Result,
+                Data = items
+            };
+            return Ok(model);
+        }
+
+        private List<CatalogItem> ChangePictureUrl(List<CatalogItem> items)
+        {
+            items.ForEach(item =>
+                item.PictureUrl = item.PictureUrl.Replace(
+                    "http://externalcatalogbaseurltobereplaced", _config["ExternalCatalogUrl"]));
+            return items;
+        }
     }
 }
